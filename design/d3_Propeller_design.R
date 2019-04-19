2e8f0bb20cadb51c0f6ff8ce730fdc52f4102139
Toryan <thomasryan1776@gmail.com>
2019-04-19 14:55:02 -0400

    d3 comments

--- design/d3_Propeller_design.R
+++ design/d3_Propeller_design.R
@@ -8,7 +8,7 @@ df10 <- readRDS("data/d3_Propeller_data.rds")
 
 ggplot(data = df10, aes(x = rpmk, y = CT)) +
   geom_jitter() +
-  facet_wrap(vars(name), as.table = FALSE) +
+  facet_wrap(vars(name), as.table = FALSE, ncol = 9) +
   theme_graphclass(font_size = 8) +
   labs(x = "Propeller Angular Velocity (Thousand RPM)", 
        y = "Thrust Coefficient",
@@ -17,12 +17,13 @@ ggplot(data = df10, aes(x = rpmk, y = CT)) +
        caption = "Source: UIUC Propeller Database, University of Illinois at Urbana-Champaign"
   ) +
   theme(text = element_text(size=8))
+  #coord_fixed(60/1)
 
 
 ggsave(filename = "d3_Propeller.png",
        path    = "figures",
        width   = 4.6,
-       height  = 6,
+       height  = 5,
        units   = "in",
        dpi     = 400)
 
